38a192c7f161a68ec765fdd6432ca19c441254b0
Freddy2 <freddy.alexis.ht@gmail.com>
2023-10-11 15:21:06 -0500

    feat: add animation to home-screen

--- app/src/main/java/com/sunday/quiz1/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sunday/quiz1/ui/home/HomeScreen.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.sunday.quiz1.R
 import com.sunday.quiz1.ui.common.AppEvent
 import com.sunday.quiz1.ui.common.MyButton
@@ -40,6 +45,7 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Animation()
         if (mem!!) {
             MyButton(
                 onclick = { homeVM.onEvent(HomeEvent.OnStart) },
@@ -68,6 +74,14 @@ fun HomeScreen(
     }
 }
 
+@Composable
+fun Animation() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie))
+    LottieAnimation(
+        modifier = Modifier.size(400.dp),
+        composition = composition
+    )
+}
 
 @Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO, showSystemUi = true, showBackground = true)
 @Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true, showBackground = true)
